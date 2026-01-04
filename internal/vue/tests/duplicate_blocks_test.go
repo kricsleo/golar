1f508fbff514830dcde890de4ad675cc73cb2bf6
auvred <aauvred@gmail.com>
2026-01-04 13:28:59 +0300

    test duplicate SFC blocks

---
+++ internal/vue/tests/duplicate_blocks_test.go
@@ -0,0 +1,75 @@
+package vue_tests
+
+import (
+	"testing"
+
+	"github.com/microsoft/typescript-go/shim/fourslash"
+	"github.com/microsoft/typescript-go/shim/lsp/lsproto"
+	"github.com/microsoft/typescript-go/shim/testutil"
+)
+
+func TestDuplicateScriptSetupDiagnostic(t *testing.T) {
+	t.Parallel()
+
+	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
+	content := withVueNodeModules(t, `// @filename: file.vue
+<script setup lang="ts">
+	const foo = 1
+</script>
+[|<script setup lang="ts">|]
+	const bar = 2
+</script>`)
+	f, done := fourslash.NewFourslash(t, nil, content)
+	defer done()
+
+	f.VerifyNonSuggestionDiagnostics(t, []*lsproto.Diagnostic{
+		{
+			Code:    &lsproto.IntegerOrString{Integer: ptrTo[int32](1_000_000)},
+			Message: "Single file component can contain only one <script setup> element.",
+		},
+	})
+}
+
+func TestDuplicateScriptDiagnostic(t *testing.T) {
+	t.Parallel()
+
+	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
+	content := withVueNodeModules(t, `// @filename: file.vue
+<script lang="ts">
+	const foo = 1
+</script>
+[|<script lang="ts">|]
+	const bar = 2
+</script>`)
+	f, done := fourslash.NewFourslash(t, nil, content)
+	defer done()
+
+	f.VerifyNonSuggestionDiagnostics(t, []*lsproto.Diagnostic{
+		{
+			Code:    &lsproto.IntegerOrString{Integer: ptrTo[int32](1_000_002)},
+			Message: "Single file component can contain only one <script> element.",
+		},
+	})
+}
+
+func TestDuplicateTemplateDiagnostic(t *testing.T) {
+	t.Parallel()
+
+	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
+	content := withVueNodeModules(t, `// @filename: file.vue
+<template>
+	<div>one</div>
+</template>
+[|<template>|]
+	<div>two</div>
+</template>`)
+	f, done := fourslash.NewFourslash(t, nil, content)
+	defer done()
+
+	f.VerifyNonSuggestionDiagnostics(t, []*lsproto.Diagnostic{
+		{
+			Code:    &lsproto.IntegerOrString{Integer: ptrTo[int32](1_000_001)},
+			Message: "Single file component can contain only one <template> element.",
+		},
+	})
+}
