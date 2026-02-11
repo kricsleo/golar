bb7b9b1f441425477e729d90bc880e19f735bb55
auvred <aauvred@gmail.com>
2026-02-11 17:09:29 +0300

    run astro bin

---
+++ packages/astro/src/bin.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+import process from 'node:process'
+import child_process from 'node:child_process'
+import { fileURLToPath } from 'node:url'
+
+const exePath = fileURLToPath(import.meta.resolve(
+  `@golar/astro-${process.platform}-${process.arch}/golar-astro${process.platform === 'win32' ? '.exe' : ''}`,
+))
+
+try {
+  child_process.execFileSync(exePath, process.argv.slice(2), {
+    stdio: 'inherit',
+  })
+} catch (e) {
+	if (e instanceof Error) {
+  	if ('status' in e && typeof e.status === 'number') {
+    	process.exit(e.status)
+		}
+	}
+  throw e
+}
+
+
