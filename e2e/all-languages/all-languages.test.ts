ad9c47a03e4931a360744f22307492baca81c75a
auvred <aauvred@gmail.com>
2026-03-01 16:56:49 +0300

    update e2e snapshot formatting

--- e2e/all-languages/all-languages.test.ts
+++ e2e/all-languages/all-languages.test.ts
@@ -48,10 +48,10 @@ test('all languages', async () => {
 		5 <button on:click={unknownVar}></button>
 		                    ~~~~~~~~~~
 
-		comp.vue:2:8 - error TS2322: Type 'number' is not assignable to type 'string'.
+		comp.vue:2:7 - error TS2322: Type 'number' is not assignable to type 'string'.
 
-		2  const vue: string = 123
-		         ~~~
+		2 const vue: string = 123
+		        ~~~
 
 		comp.vue:6:10 - error TS2339: Property 'unknownVar' does not exist on type 'ComponentPublicInstance<{}, {}, {}, {}, {}, {}, {}, {}, false, ComponentOptionsBase<any, any, any, any, any, any, any, any, any, {}, {}, string, {}, {}, {}, string, ComponentProvideOptions>, ... 5 more ..., any>'.
 
